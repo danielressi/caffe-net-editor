09b2b31b39a5dad80a83ade2b9435ae767b1ac8e
Daniel Ressi <danielressi@Daniels-MacBook-Pro.local>
2017-06-20 03:55:10 +0100

    added setup.py and examples

---
+++ proto_editor/__init__.py
@@ -0,0 +1,3 @@
+from proto_editor import ProtoNetEditor
+
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='proto_editor',
+      version='0.1',
+      description='Simple API to modify existing caffe nets',
+      url='https://github.com/danielressi/caffe-net-editor.git',
+      author='daniel ressi',
+      author_email='-',
+      license='MIT',
+      packages=['proto_editor'],
+      zip_safe=False)
+
+
